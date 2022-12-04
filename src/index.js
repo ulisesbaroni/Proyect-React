5f8638672fa58e6b94c1ba3b1d467b9ef516aedd
Ulises Baroni <ulisesbaroni@gmail.com>
2022-12-04 00:05:47 -0300

    Se eliminaron lineas de codigo y archivos innecesarios

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-//import './assets/test.js'
-
-/* import AppClock from './components/Clock/Clock'; */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
