e81a0983a149a289c90c6074a214ae4d7aee9fd6
Ulises Baroni <ulisesbaroni@gmail.com>
2022-12-06 13:12:16 -0300

    se corrigieron estilos

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -6,7 +6,7 @@ const Item = ({prod}) => {
         <div className="card cardProducto shadow p-3 mb-5 bg-body rounded" key={prod.id}>
                         <img src={prod.img} className="card-img-top" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">{prod.nombre}</h5>
+                            <h5 className="card-title" style={{minHeight: '39px'}}>{prod.nombre}</h5>
                             <p className="card-text">Dimensiones: {prod.dimensiones}</p>
                             <p className="card-text">Precio: ${new Intl.NumberFormat('de-DE').format(prod.precio)}</p>
                             <p className="card-text">Stock: {prod.stock}</p>
